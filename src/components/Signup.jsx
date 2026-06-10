7c94b2de75fd26eaad9a0a4062bd071fb7906757
mario <mario_reyes505@hotmail.com>
2026-06-09 22:32:22 -0500

    add a basic form action function to Signup.jsx. Also removed leftover code

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,6 +1,11 @@
 export default function Signup() {
+  const signupAction = (formData) => {
+    const enteredEmail = formData.get("email");
+    console.log(enteredEmail);
+  };
+
   return (
-    <form>
+    <form action={signupAction}>
       <h2>Welcome on board!</h2>
       <p>We just need a little bit of data from you to get you started 🚀</p>
 
