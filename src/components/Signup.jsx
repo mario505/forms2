a1b74d599197bb5112b0043fa6e8225b51565b48
mario <mario_reyes505@hotmail.com>
2026-06-10 22:17:07 -0500

    add logic to get all fields using form actions, and validate and store basic errors on an internal array

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,7 +1,44 @@
+import {
+  hasMinLength,
+  isEmail,
+  isEqualToOtherValue,
+  isNotEmpty,
+} from "../util/validation.js";
+
 export default function Signup() {
   const signupAction = (formData) => {
-    const enteredEmail = formData.get("email");
-    console.log(enteredEmail);
+    const email = formData.get("email");
+    const password = formData.get("password");
+    const confirmPassword = formData.get("confirm-password");
+    const firstName = formData.get("first-name");
+    const lastName = formData.get("last-name");
+    const role = formData.get("role");
+    const terms = formData.get("terms");
+    const acquisitionChannel = formData.getAll("acquisition");
+
+    let errors = [];
+
+    if (!isEmail(email)) {
+      errors.push("Invalida Email address.");
+    }
+    if (!isNotEmpty(password) || !hasMinLength(password, 6)) {
+      errors.push("You must provide a password with at least 6 characters");
+    }
+    if (!isEqualToOtherValue(password, confirmPassword)) {
+      errors.push("Passwords do not match");
+    }
+    if (!isNotEmpty(firstName) || !isNotEmpty(lastName)) {
+      errors.push("Please provide both your first and last name");
+    }
+    if (!isNotEmpty(role)) {
+      errors.push("Please select a role");
+    }
+    if (!terms) {
+      errors.push("You must agree to the terms and conditions");
+    }
+    if (acquisitionChannel.length === 0) {
+      errors.push("Please select at least 1 acquisition channel");
+    }
   };
 
   return (
